a2184e29362e34ceff7b26c443c40027a712249d
ali ghelej beigi <92574228+alibeigi95@users.noreply.github.com>
2023-10-18 17:31:20 +0330

    feat: init

---
+++ example/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hive Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Column(
+        children: [
+          Text('test'),
+        ],
+      ),
+    );
+  }
+}
+
